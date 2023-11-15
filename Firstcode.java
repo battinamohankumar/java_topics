15c2085f85a1603dcce3392a233be116e08ceffc
battinamohankumar <149995715+battinamohankumar@users.noreply.github.com>
2023-11-15 14:15:55 +0530

    Add files via upload

---
+++ Firstcode.java
@@ -0,0 +1,13 @@
+package First_Package;
+
+public class Firstcode {
+
+	public static void main(String[] args) {
+	System.out.println(" Hello Team, Good Morning.");
+	System.out.println(" I Mohan working in Sightspectrum.");
+	System.out.println(" I'm glad to work with you on java.");
+	System.out.println("\n Thank you Team.");
+
+	}
+
+}
